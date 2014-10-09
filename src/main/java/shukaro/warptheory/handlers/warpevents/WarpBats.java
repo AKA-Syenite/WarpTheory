d6f35fe97acd0d491b498fb9a81bfdacebb7513c
Robert Antosh <robert@antosh.net>
2014-10-09 14:52:12 -0500

    Changed WarpBats to use the new helper funcs in MiscHelper.

--- src/main/java/shukaro/warptheory/handlers/warpevents/WarpBats.java
+++ src/main/java/shukaro/warptheory/handlers/warpevents/WarpBats.java
@@ -8,6 +8,7 @@ import shukaro.warptheory.WarpTheory;
 import shukaro.warptheory.handlers.IWarpEvent;
 import shukaro.warptheory.util.ChatHelper;
 import shukaro.warptheory.util.FormatCodes;
+import shukaro.warptheory.util.MiscHelper;
 
 public class WarpBats implements IWarpEvent
 {
@@ -27,12 +28,21 @@ public class WarpBats implements IWarpEvent
     public boolean doEvent(World world, EntityPlayer player)
     {
         ChatHelper.sendToPlayer(player, FormatCodes.Purple.code + FormatCodes.Italic.code + StatCollector.translateToLocal("chat.warptheory.batsstart"));
+        /*
         if (!player.getEntityData().hasKey(WarpTheory.modID))
             player.getEntityData().setTag(WarpTheory.modID, new NBTTagCompound());
         if (player.getEntityData().getCompoundTag(WarpTheory.modID).hasKey("bats"))
             player.getEntityData().getCompoundTag(WarpTheory.modID).setInteger("bats", player.getEntityData().getCompoundTag(WarpTheory.modID).getInteger("bats") + 15 + world.rand.nextInt(30));
         else
             player.getEntityData().getCompoundTag(WarpTheory.modID).setInteger("bats", 15 + world.rand.nextInt(30));
+        return true;
+        */
+        //Set tag on player inside the WarpTheory compound tag.
+        //Creates compound if it does not exist.
+        //Creates tag if it does not exist.
+        //If tag exists, value is added.
+        //Should probably name this better than "setTag" since it increments if exists.  "addToTag"?
+        MiscHelper.setTag(player, "bats", 15 + world.rand.nextInt(30));
         return true;
     }
 }
