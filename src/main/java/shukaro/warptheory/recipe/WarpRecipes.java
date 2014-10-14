0c985d33fda3ae991aa96700ea7c44f112656a7a
Ross Swartz <ross.swartz@gmail.com>
2014-10-14 17:42:43 -0400

    Meat

--- src/main/java/shukaro/warptheory/recipe/WarpRecipes.java
+++ src/main/java/shukaro/warptheory/recipe/WarpRecipes.java
@@ -8,13 +8,28 @@ import thaumcraft.api.ThaumcraftApi;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 
+import java.util.ArrayList;
+
 public class WarpRecipes
 {
+    private static ArrayList<ItemStack> meats = new ArrayList<ItemStack>();
+
+    private static void initMeats()
+    {
+        meats.add(new ItemStack(Items.chicken));
+        meats.add(new ItemStack(Items.beef));
+        meats.add(new ItemStack(Items.porkchop));
+        meats.add(new ItemStack(Items.fish));
+    }
+
     public static void initRecipes()
     {
+        initMeats();
         ThaumcraftApi.addInfusionCraftingRecipe("WARPCLEANSER", new ItemStack(WarpItems.itemCleanser), 5, new AspectList().add(Aspect.ELDRITCH, 15).add(Aspect.EXCHANGE, 15), new ItemStack(Items.ghast_tear),
                 new ItemStack[]{ItemApi.getItem("itemResource", 14), ItemApi.getItem("itemResource", 14), new ItemStack(Items.quartz), new ItemStack(Items.quartz)});
         ThaumcraftApi.addInfusionCraftingRecipe("WARPAMULET", new ItemStack(WarpItems.itemAmulet), 10, new AspectList().add(Aspect.ELDRITCH, 15).add(Aspect.EXCHANGE, 15).add(Aspect.MAGIC, 10), new ItemStack(WarpItems.itemCleanser),
                 new ItemStack[]{new ItemStack(Items.gold_ingot), new ItemStack(Items.gold_ingot), ItemApi.getItem("itemResource", 2), ItemApi.getItem("itemResource", 2)});
+        for (ItemStack meat : meats)
+            ThaumcraftApi.addCrucibleRecipe("WARPSOMETHING", new ItemStack(WarpItems.itemSomething), meat, new AspectList().add(Aspect.ELDRITCH, 8));
     }
 }
--- src/main/java/shukaro/warptheory/research/WarpResearch.java
+++ src/main/java/shukaro/warptheory/research/WarpResearch.java
@@ -7,6 +7,7 @@ import shukaro.warptheory.items.WarpItems;
 import thaumcraft.api.ItemApi;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
+import thaumcraft.api.crafting.CrucibleRecipe;
 import thaumcraft.api.crafting.InfusionRecipe;
 import thaumcraft.api.research.ResearchItem;
 import thaumcraft.api.research.ResearchPage;
@@ -30,14 +31,15 @@ public class WarpResearch
         researchAmulet = new ResearchItem("WARPAMULET", "ELDRITCH", new AspectList().add(Aspect.ELDRITCH, 6).add(Aspect.EXCHANGE, 3).add(Aspect.MAGIC, 3), -3, 4, 3, new ItemStack(WarpItems.itemAmulet))
                 .setPages(new ResearchPage[]{
                         new ResearchPage(StatCollector.translateToLocal("warptheory.warpamulet.research")),
-                        new ResearchPage(new InfusionRecipe("WARPAMULET", new ItemStack(WarpItems.itemAmulet), 10, new AspectList().add(Aspect.ELDRITCH, 15).add(Aspect.EXCHANGE, 15).add(Aspect.MAGIC, 10), new ItemStack(WarpItems.itemCleanser),
-                                new ItemStack[]{ItemApi.getItem("itemResource", 2), new ItemStack(Items.gold_ingot), ItemApi.getItem("itemResource", 2), new ItemStack(Items.gold_ingot)}))
+                        new ResearchPage(new InfusionRecipe("WARPAMULET", new ItemStack(WarpItems.itemAmulet), 10, new AspectList().add(Aspect.ELDRITCH, 15).add(Aspect.EXCHANGE, 15).add(Aspect.MAGIC, 30), new ItemStack(WarpItems.itemCleanser),
+                                new ItemStack[]{ItemApi.getItem("itemResource", 2), new ItemStack(Items.gold_ingot), ItemApi.getItem("itemResource", 2), new ItemStack(Items.gold_ingot), new ItemStack(Items.diamond), new ItemStack(Items.diamond), new ItemStack(Items.diamond), new ItemStack(Items.diamond)}))
                 }).setParents("WARPCLEANSER");
         researchAmulet.registerResearchItem();
 
         researchSomething = new ResearchItem("WARPSOMETHING", "ALCHEMY", new AspectList().add(Aspect.ELDRITCH, 3), -2, -5, 2, new ItemStack(WarpItems.itemSomething))
                 .setPages(new ResearchPage[]{
-                        new ResearchPage(StatCollector.translateToLocal("warptheory.warpsomething.research"))
+                        new ResearchPage(StatCollector.translateToLocal("warptheory.warpsomething.research")),
+                        new ResearchPage(new CrucibleRecipe("WARPSOMETHING", new ItemStack(WarpItems.itemSomething), new ItemStack(Items.beef), new AspectList().add(Aspect.ELDRITCH, 8)))
                 }).setHidden().setAspectTriggers(Aspect.ELDRITCH);
         researchSomething.registerResearchItem();
     }
